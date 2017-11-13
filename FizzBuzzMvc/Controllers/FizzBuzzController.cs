585b677c36fdc21b0983074def58317bab090233
Jay Jackson <jaynickjackson@gmail.com>
2017-11-13 11:17:05 -0500

    made proper

--- FizzBuzzMvc/Controllers/FizzBuzzController.cs
+++ FizzBuzzMvc/Controllers/FizzBuzzController.cs
@@ -8,10 +8,16 @@ namespace FizzBuzzMvc.Controllers
 {
     public class FizzBuzzController : Controller
     {
+        private string ToFizzBuzz(int value)
+        {
+            return value % 15 == 0 ? "FizzBuzz" : value % 5 == 0 ? "Buzz" : value % 3 == 0 ? "Fizz" : $"{value}";
+        }
+
         // GET: FizzBuzz
         public ActionResult Index()
         {
-            return View();
+            var model = Enumerable.Range(1, 100).Select(ToFizzBuzz);
+            return View(model);
         }
     }
 }
